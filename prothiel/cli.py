9cd95b9e26038acc5d55574c8f4fe505bc44542b
Maki <sunwood.ai.labs@gmail.com>
2024-05-10 22:19:16 +0900

    🛠️ [fix] prothiel/cli.pyのコードブロック表示を改善
    詳細な説明：
    - `prothiel/cli.py` のコードブロックの表示形式を改善し、出力時の視認性を向上させました。
    - コードブロックの区切りをより明確にするための修正を行いました。

--- prothiel/cli.py
+++ prothiel/cli.py
@@ -1,15 +1,27 @@
 import argparse
 from .extract import extract_code_blocks
+from .run_tests import run_tests
+import os
+from art import *
+import prothiel
+import sys
 
 def main():
-    parser = argparse.ArgumentParser(description='Extract code blocks from markdown and organize them into Python files.')
-    parser.add_argument('markdown_file', help='Path to the markdown file')
-    parser.add_argument('root_path', help='Root path for the extracted files')
-    parser.add_argument('--file-path-pattern', default=r'### (.*?)\n', help='Regex pattern for file paths')
-    parser.add_argument('--code-block-pattern', default=r'```python\n(.*?)\n```', help='Regex pattern for code blocks')
+    parser = argparse.ArgumentParser(description='MarkdownファイルからコードブロックをPythonファイルとして抽出・整理します。')
+    parser.add_argument('--markdown_file', default='.Prothiel.md', help='読み込むMarkdownファイルのパス')
+    parser.add_argument('--root_path', default='./', help='抽出したファイルを保存するルートパス')
+    parser.add_argument('--file-path-pattern', default=r'### (.*?)\n', help='ファイルパスを抽出するための正規表現パターン')
+    parser.add_argument('--code-block-pattern', default=r'```python\n(.*?)\n```', help='コードブロックを抽出するための正規表現パターン')
     args = parser.parse_args()
+    
+    tprint("Prothiel - v{:}".format(str(prothiel.__version__)))
+    
+    sys.path.append(args.root_path)
 
     with open(args.markdown_file, 'r', encoding='utf-8') as file:
         markdown_content = file.read()
 
     extract_code_blocks(markdown_content, args.root_path, args.file_path_pattern, args.code_block_pattern)
+    run_tests(args.root_path)
+    
+    tprint("!! successfully !!")
