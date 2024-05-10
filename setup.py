4ae0107dec57e7dae28140c0316db14e29fe0258
Maki <sunwood.ai.labs@gmail.com>
2024-05-10 22:49:10 +0900

    🔧 [chore] setup.pyのバージョン番号を同期更新
    詳細な説明：
    - `setup.py` 内のバージョン指定を「0.1.6」から「0.1.8」に更新しました。
    - これにより、パッケージバージョンが最新の状態に保たれ、ユーザーが最新の機能を利用できるようになります。

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='prothiel',
-    version="0.1.6",
+    version="0.1.8",
     # PyPIに表示される長い説明文
     long_description=long_description,
     # 長い説明文のフォーマット
