b7b4efdceec0f264e5dae61e4603a5ad376d69c3
Maki <sunwood.ai.labs@gmail.com>
2024-05-10 19:56:19 +0900

    🔧 [fix] setup.pyのバージョン番号を修正
    - `setup.py`において、パッケージ`prothiel`のバージョン番号を`0.2.0`から`0.1.0`に修正
    - バージョン管理に関する誤りを修正し、適切なリリース番号に更新しています

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='prothiel',
-    version='0.2.0',
+    version='0.1.0',
     description='A package to extract code blocks from markdown and organize them into Python files',
     author='Maki',
     author_email='sunwood.ai.labs@gmail.com',
